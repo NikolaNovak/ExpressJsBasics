583cc93f3e9a836c62dfb05746182cc366d4afb6
Nikola Novak <nikola.novak.hr@gmail.com>
2021-01-01 21:26:32 +0100

    Added a GET API which returns members & a simple logging middleware

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ExpressJsBasics
-Learning Express JS Basics
+
+Learning Express JS Basics.
+
+## Serve on localhost:5000 with nodemon using the following command:
+
+> npm run dev
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+const logger = (req, res, next) => {
+  console.log("Hello");
+  next();
+};
+
+// Init middleware
+app.use(logger);
+
+// Gets all members
+app.get("/api/members", (req, res) => res.json(members));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
