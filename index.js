dc001cce14e970f23fd5b52dd49f1aa7cc82956f
Nikola Novak <nikola.novak.hr@gmail.com>
2021-01-01 21:05:51 +0100

    Set up a static server & added nodemon

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
