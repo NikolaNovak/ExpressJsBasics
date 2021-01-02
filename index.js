6e50b4d4c591d401c8245ef09e330c6fd22b8d2c
Nikola Novak <nikola.novak.hr@gmail.com>
2021-01-02 18:54:32 +0100

    Replaced the static folder with the handlebars template engine

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
+const members = require("./Members");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Handlebars middleware
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+// Homepage Route
+app.get("/", (req, res) => res.render("index", { title: "Member App", members }));
 
 // Logger middleware
 app.use(logger);
@@ -13,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 // Members API routes
 app.use("/api/members", require("./routes/api/members"));
